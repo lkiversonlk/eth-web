0eb8c3d96f0a73ee52cea3d2dfadc2be02ffd764
liukan <liukan@bytedance.com>
2017-08-19 03:51:05 +0800

    add ssl
    Change-Id: I8fccf5968af5377261ca6565bf7377c02de00b78

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const { createServer } = require('http')
+const { createServer } = require('https')
 const { parse } = require('url')
 const { join } = require('path')
+var fs = require('fs')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -12,9 +13,17 @@ const rootStaticFiles = [
   '/assets',
 ]
 
+var ssl = {
+  key: fs.readFileSync('./ssl/liukan.key', 'utf8'),
+  cert: fs.readFileSync('./ssl/www.94eth.com.crt', 'utf8'),
+  ca: [fs.readFileSync('./ssl/intermediate.crt', 'utf8'),
+    fs.readFileSync('./ssl/root.crt', 'utf8')],
+  passphrase: 'asdffa'
+}
+
 app.prepare()
 .then(() => {
-  createServer((req, res) => {
+  createServer(ssl, (req, res) => {
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
     if (rootStaticFiles.filter((file) => pathname.indexOf(file) > -1).length > 0) {
