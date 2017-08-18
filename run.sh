cc220628a83e3fb6e524b9ff46540f6a8f21dd1d
liukan <liukan@bytedance.com>
2017-08-18 19:27:14 +0800

    run
    Change-Id: Ibd46a3a8a457e55a28cf885b85a634f1a2e17782

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ ps aux | grep "node server.js" | tr -s " " | cut -d" " -f2 | xargs kill
 git pull
 npm install || true
 npm run build:content
-nohup NODE_ENV=production node server.js 2>&1 &
+NODE_ENV=production nohup node server.js 2>&1 &
